b543b8c494b01d2c61ab68ca45531998fdf8c617
sarah <sarah@triggersandsparks.com>
2015-12-25 18:11:05 +0000

    Parse & output data from geocoder control results; fixes #2.

--- includes/admin/js/geolocator.js
+++ includes/admin/js/geolocator.js
@@ -4,67 +4,75 @@
 		console.log( 'No key set.' );
 
 	} else {
-		// Initialise map
+		// Initialise map variables and settings
 		L.mapbox.accessToken = $( '#wanderlist-geolocation-input' ).data( 'mapboxkey' );
-		var map = L.mapbox.map( 'wanderlist-geolocation-map', 'mapbox.streets' )
-	    .addControl( L.mapbox.geocoderControl( 'mapbox.places', {
-	        autocomplete: true,
-					keepOpen: true
-	    } ) );
+		var map = L.mapbox.map( 'wanderlist-geolocation-map', 'mapbox.streets' );
+		map.scrollWheelZoom.disable(); // Disable zoom
+		var geocoderControl = L.mapbox.geocoderControl( 'mapbox.places', {
+	      autocomplete: true,
+				keepOpen: true
+	  } );
 
-		// Disable zoom when scrolling
-		map.scrollWheelZoom.disable();
+		// When the user selects a place from our drop-down, parse that data into something we can use
+		geocoderControl.on( 'select', function( event ) {
+			parseLocationData( event.feature );
+		} );
 
-		// Start geocoding!
-		$( '#wanderlist-geolocation-input' ).on('change', function() {
-			var locationString = $( this ).attr( 'value' );
-			var geocodeURI = 'https://api.mapbox.com/v5/geocode/mapbox.places/' + encodeURI( locationString ) + '.json?access_token=' + accessToken;
-			$.ajax(geocodeURI, {
-				success: function(response) {
-					var result = response.features[0];
-					var selected = result.context;
-					// If the context object doesn't exist, we're dealing with a limited dataset. Let's get what we can.
-					if (!selected) {
-						if ( result.id.match( /^country/ ) ) {
-							var country = result.text;
-						} else if ( result.id.match( /^region/ ) ) {
-							var region = result.text;
-						}
-					// We have a full result set, so let's parse its data
-					} else {
-						var city = result.text;
-						for (var i = 0; i < selected.length; i++) {
-							if ( selected[i].id.match( /^country/ ) ) {
-								var country = selected[i].text;
-							} else if ( selected[i].id.match( /^region/ ) ) {
-								var region = selected[i].text;
-							}
-						}
-					}
-					var lng = response.features[0].center[0];
-					var lat = response.features[0].center[1];
-					$( '#wanderlist-city' ).val( city );
-					$( '#wanderlist-region' ).val( region );
-					$( '#wanderlist-country' ).val( country );
-					$( '#wanderlist-lng' ).val( lng );
-					$( '#wanderlist-lat' ).val( lat );
-					var placeName = response.features[0].place_name;
+		// When we have some kind of error, show an error message
+		geocoderControl.on( 'error', function( event ) {
+			$( '#wanderlist-geocoder-message' ).removeClass( 'success' );
+			$( '#wanderlist-geocoder-message' ).addClass( 'error' );
+			console.log( event.error );
+			$( '#wanderlist-geocoder-message' ).find( '.place' ).text( 'ERROR' );
+		} );
 
-					// Vatican doesn't return the correct results, so we're going to do it manually
-					if ( 'Città del Vaticano' === city ) {
-						$( '#wanderlist-region' ).val( '' );
-						$( '#wanderlist-country' ).val( 'Vatican City' );
-						placeName = 'Città del Vaticano';
-					}
-
-					$( '#wanderlist-geocoder-message' ).addClass( 'success' );
-					$( '#wanderlist-geocoder-message' ).find( '.place' ).text( placeName );
-				},
-				error: function(response) {
-					$( '#wanderlist-geocoder-message' ).addClass( 'error' );
-					$( '#wanderlist-geocoder-message' ).find( '.place' ).text( 'ERROR' );
-				}
-			})
-		});
+		// Add the control to our map
+		map.addControl( geocoderControl );
 	}
+
+	// Function to process & store the results of our geocoder.
+	function parseLocationData( feature ) {
+		// If the context object doesn't exist, we're dealing with a limited dataset. Let's get what we can.
+		if ( !feature.context ) {
+			if ( feature.id.match( /^country/ ) ) {
+				var country = feature.text;
+			} else if ( feature.id.match( /^region/ ) ) {
+				var region = feature.text;
+			}
+		// Otherwise, we have a full result set, so let's parse its data
+		} else {
+			var city = feature.text;
+			for (var i = 0; i < feature.context.length; i++) {
+				if ( feature.context[i].id.match( /^country/ ) ) {
+					var country = feature.context[i].text;
+				} else if ( feature.context[i].id.match( /^region/ ) ) {
+					var region = feature.context[i].text;
+				}
+			}
+		}
+		var lng = feature.center[0];
+		var lat = feature.center[1];
+
+		// Output our values to our hidden form fields
+		$( '#wanderlist-city' ).val( city );
+		$( '#wanderlist-region' ).val( region );
+		$( '#wanderlist-country' ).val( country );
+		$( '#wanderlist-lng' ).val( lng );
+		$( '#wanderlist-lat' ).val( lat );
+		var placeName = feature.place_name;
+
+		// Since Geocoder results for Vatican aren't accurate, we'll overwrite them
+		if ( 'Città del Vaticano' === city ) {
+			$( '#wanderlist-region' ).val( '' );
+			$( '#wanderlist-country' ).val( 'Vatican City' );
+			placeName = 'Città del Vaticano';
+		}
+
+		// Show a friendly success message
+		$( '#wanderlist-geocoder-message' ).removeClass( 'error' );
+		$( '#wanderlist-geocoder-message' ).addClass( 'success' );
+		$( '#wanderlist-geocoder-message' ).find( '.place' ).text( placeName );
+
+	}
+
 } )( jQuery );
