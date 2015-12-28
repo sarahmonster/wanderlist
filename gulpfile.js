1bb15f215a2ab933a1efcbdef853372288423be1
sarah <sarah@triggersandsparks.com>
2015-12-28 17:49:21 +0000

    Clean up unused gulp dependencies and add livereload and source maps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,31 @@
 // Load plugins
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var del = require('del');
+var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
+var autoprefixer = require( 'gulp-autoprefixer' );
+var del = require( 'del' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+var livereload = require( 'gulp-livereload' );
 
-gulp.task('sass', function() {
-  return gulp.src('./assets/scss/style.scss')
-    .pipe(sass({ style: 'expanded' }).on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('./includes/public/css'));
+
+gulp.task( 'sass', function() {
+  return gulp.src( './assets/scss/style.scss' )
+    .pipe( sass( { style: 'expanded' } ).on( 'error', sass.logError ) )
+    .pipe( autoprefixer( { browsers: ['last 2 versions', 'ie >= 9'], cascade: false } ) )
+    .pipe( sourcemaps.write( './', { includeContent: false, sourceRoot: 'source' } ) )
+    .pipe( gulp.dest( './includes/public/css' ) )
+    .pipe( livereload() );
 });
 
-gulp.task('clean', function(done) {
-  del(['./includes/public/css/style.css'], done);
+gulp.task( 'clean', function( done ) {
+  del( ['./includes/public/css/style.css'], done );
 });
 
-gulp.task('build', ['clean', 'sass']);
+gulp.task( 'build', ['clean', 'sass'] );
 
-gulp.task('watch', function() {
+gulp.task( 'watch', function() {
   // Watch .scss files
-  gulp.watch('./assets/scss/**/*.scss', ['sass']);
+  gulp.watch( './assets/scss/**/*.scss', ['sass'] );
 });
 
 // Default task is watch
-gulp.task('default', ['build', 'watch'])
+gulp.task( 'default', ['build', 'watch'] )
