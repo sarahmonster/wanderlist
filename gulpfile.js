712805b186c9ab2be629f040bf5ebdc0c94f8b65
sarah <sarah@triggersandsparks.com>
2015-12-29 12:36:32 +0000

    Use CSS Comb to alphabetize properties and clean up generated CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ var autoprefixer = require( 'gulp-autoprefixer' );
 var del = require( 'del' );
 var sourcemaps = require( 'gulp-sourcemaps' );
 var livereload = require( 'gulp-livereload' );
-
+var csscomb = require( 'gulp-csscomb' );
 
 gulp.task( 'sass', function() {
   return gulp.src( './assets/scss/style.scss' )
     .pipe( sass( { style: 'expanded' } ).on( 'error', sass.logError ) )
     .pipe( autoprefixer( { browsers: ['last 2 versions', 'ie >= 9'], cascade: false } ) )
     .pipe( sourcemaps.write( './', { includeContent: false, sourceRoot: 'source' } ) )
+    .pipe( csscomb() )
     .pipe( gulp.dest( './includes/public/css' ) )
     .pipe( livereload() );
 });
